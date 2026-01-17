86d412835c36312c613ec374f6ff99e13f87d0ac
Philippe <philippe.sa.costa@gmail.com>
2026-01-17 21:55:13 +0100

    add empty string as default for config

--- backend/src/config.py
+++ backend/src/config.py
@@ -3,10 +3,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    OPENWEATHER_API_URI: str = Field(...)
-    OPENWEATHER_API_KEY: SecretStr = Field(...)
+    OPENWEATHER_API_URI: str = Field("")
+    OPENWEATHER_API_KEY: SecretStr = Field("")
     WEATHER_UPDATE_INTERVAL_MINUTES: int = Field(5, ge=2)
-    DATABASE_URL: str = Field(...)
+    DATABASE_URL: str = Field("")
 
     model_config = SettingsConfigDict(
         env_file="../../.env", env_file_encoding="utf-8", extra="ignore"
